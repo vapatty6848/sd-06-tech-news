08f0f372dc94810d2fd3eedeb862ce54b760e0ee
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-05-13 17:55:15 -0300

    :tada: Starting first python project :happy:

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,16 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+    except requests.ReadTimeout:
+        return None
+    return response.text if response.status_code == 200 else None
 
 
 # Requisito 2
