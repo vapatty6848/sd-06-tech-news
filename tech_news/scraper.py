664a2e8ce4751f837c5f7d8e4ee919e861267bc9
William <williamjog@hotmail.com>
2021-05-13 13:23:45 -0300

    1º Requisito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+import requests
+from time import sleep
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        sleep(1)
+
+        if response.status_code != 200:
+            return None
+
+        return response.text
+
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
