68669858b4bde706e3a09e1fa4c7a98a83402aa4
isabellavjs <isabellavjs@gmail.com>
2021-05-13 15:45:15 -0300

    Requirement 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,25 @@
+import time
+import requests
+from requests.exceptions import ReadTimeout
+
+"""
+A função utiliza o método get() da biblioteca requests
+A função executada com uma URL correta retorna o conteúdo html
+A função, sofrendo timeout, retorna None
+A função retorna None quando recebe uma resposta com código diferente de 200
+A função respeita o rate limit
+"""
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+    except ReadTimeout:
+        return None
+
+    return response.text if response.status_code == 200 else None
 
 
 # Requisito 2
