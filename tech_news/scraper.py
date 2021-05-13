6f60232b05af2ff98d4f0dbeb6f7a19fe4a1ec87
LeandroParisi <lepcbelisario@gmail.com>
2021-05-13 17:52:34 -0300

    wip: requirement 2 - get summary

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -23,34 +23,52 @@ def scrape_noticia(html_content):
     selector = Selector(text=html_content, base_url=html_content)
     url = selector.css("head > link[rel=canonical]::attr(href)").get()
     title = selector.css("h1#js-article-title::text").get()
-    timestamp = ""
-    writer = ""
-    shares_count = ""
-    comments_count = ""
-    summary = ""
-    sources = ""
-    categories = ""
-    # timestamp = selector.css("time[datetime]")
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+    writer = selector.css("a.tec--author__info__link::text").get() or None
+    shares_count = (
+        selector.css("div.tec--toolbar__item::text").get().split()[0] or 0
+    )
+    comments_count = selector.css(
+        "div.tec--toolbar__item #js-comments-btn::attr(data-count)"
+    ).get()
+
+    summary = selector.css(".tec--article__body p *::text").getall()
+    treated_summary = "".join(summary)
+    # summary_selector = Selector(text=summary)
+    # summary_links = summary_selector.css("a").getall()
+
+    # sources = selector.css()
+    # categories = selector.css()
     print(url)
     print(title)
+    print(timestamp)
+    print(writer)
+    print(shares_count)
+    print(comments_count)
+    print(treated_summary)
+    # print(summary_links)
 
-    # pass
-    #     {
-    #   "url": "https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/155000-musk-tesla-carros-totalmente-autonomos.htm",
-    #   "title": "Musk: Tesla está muito perto de carros totalmente autônomos",
-    #   "timestamp": "2020-07-09T11:00:00",
-    #   "writer": "Nilton Kleina",
-    #   "shares_count": 61,
-    #   "comments_count": 26,
-    #   "summary": "O CEO da Tesla, Elon Musk, garantiu que a montadora está muito perto de atingir o chamado nível 5 de autonomia de sistemas de piloto automático de carros. A informação foi confirmada em uma mensagem enviada pelo executivo aos participantes da Conferência Anual de Inteligência Artificial (WAIC, na sigla em inglês). O evento aconteceu em Xangai, na China, onde a montadora comemora resultados positivos de mercado.",
-    #   "sources": ["Venture Beat"],
-    #   "categories": [
-    #     "Mobilidade Urbana/Smart Cities",
-    #     "Veículos autônomos",
-    #     "Tesla",
-    #     "Elon Musk"
-    #   ]
-    # }
+    # print(sources)
+    # print(categories)
+
+
+# pass
+#     {
+#   "url": "https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/155000-musk-tesla-carros-totalmente-autonomos.htm",
+#   "title": "Musk: Tesla está muito perto de carros totalmente autônomos",
+#   "timestamp": "2020-07-09T11:00:00",
+#   "writer": "Nilton Kleina",
+#   "shares_count": 61,
+#   "comments_count": 26,
+#   "summary": "O CEO da Tesla, Elon Musk, garantiu que a montadora está muito perto de atingir o chamado nível 5 de autonomia de sistemas de piloto automático de carros. A informação foi confirmada em uma mensagem enviada pelo executivo aos participantes da Conferência Anual de Inteligência Artificial (WAIC, na sigla em inglês). O evento aconteceu em Xangai, na China, onde a montadora comemora resultados positivos de mercado.",
+#   "sources": ["Venture Beat"],
+#   "categories": [
+#     "Mobilidade Urbana/Smart Cities",
+#     "Veículos autônomos",
+#     "Tesla",
+#     "Elon Musk"
+#   ]
+# }
 
 
 scrape_noticia(
