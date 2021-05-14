83533a98a589844afc89c280431d27b832179961
Tiago Berwanger <berwangertiago@gmail.com>
2021-05-13 22:34:19 -0300

    requirement 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,4 +1,5 @@
 import requests
+import parsel
 import time
 from requests.exceptions import Timeout
 
@@ -18,7 +19,44 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+
+    URL = selector.css("head link[rel=canonical]::attr(href)").get()
+    TITLE = selector.css("h1::text").get()
+    TIMESTAMP = selector.css(
+        ".tec--timestamp__item"
+    ).xpath(
+        "./time/@datetime"
+    ).get().strip()
+    WRITER = selector.css(".z--font-bold a::text").get().strip()
+    SHARES_COUNT_UNFORMATTED = selector.css(
+        ".tec--toolbar__item::text"
+    ).get().split(' ')[1].strip()
+    SHARES_COUNT = int(SHARES_COUNT_UNFORMATTED)
+    COMMENTS_COUNT_UNFORMATTED = selector.css(
+        "#js-comments-btn::attr(data-count)"
+    ).get()
+    COMMENTS_COUNT = int(COMMENTS_COUNT_UNFORMATTED)
+    SUMMARY_UNFORMATTED = selector.css(
+        ".tec--article__body > p:nth-child(1) *::text"
+    ).getall()
+    SUMMARY = ''.join(SUMMARY_UNFORMATTED)
+    GET_SOURCES = selector.css(".z--mb-16 .tec--badge::text").getall()
+    SOURCES = [source.strip() for source in GET_SOURCES]
+    GET_CATEGORIES = selector.css("#js-categories > a *::text").getall()
+    CATEGORIES = [category.strip() for category in GET_CATEGORIES]
+    result_dict = {
+        'url': URL,
+        'title': TITLE,
+        'timestamp': TIMESTAMP,
+        'writer': WRITER,
+        'shares_count': SHARES_COUNT,
+        'comments_count': COMMENTS_COUNT,
+        'summary': SUMMARY,
+        'sources': SOURCES,
+        'categories': CATEGORIES
+    }
+    return result_dict
 
 
 # Requisito 3
