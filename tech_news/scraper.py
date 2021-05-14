03f93f5b4316273b2db2219daa95dd2c6f289ea3
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-05-13 22:14:38 -0300

    requirements 1 and 2... or the attempt

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,57 @@
+from parsel import Selector
+import requests
+import time
+
+tecmundo = "https://www.tecmundo.com.br/novidades"
+
+
+def remove_spaces(items):
+    return [item[1:-1] for item in items]
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        result = requests.get(url, timeout=3)
+    except requests.ReadTimeout:
+        return None
+    else:
+        status = result.status_code
+        if status != 200:
+            return None
+        return result.text
 
 
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css('head link[rel=canonical]::attr(href)').get()
+    title = selector.css('.tec--article__header__title::text').get()
+    timestamp = selector.css('time::attr(datetime)').get()
+    writer = selector.css('.tec--author__info__link::text').get()
+    shares_count = int(selector.css('.tec--toolbar__item::text').get()[1] or 0)
+    comments_count = int(
+        selector.css('#js-comments-btn::attr(data-count)').get()
+    )
+    summary = selector.css('head meta[name=description]::attr(content)').get()
+    sources = remove_spaces(selector.css('.z--mb-16 a::text').getall())
+    categories = remove_spaces(selector.css('#js-categories a::text').getall())
+
+    obj = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer[1:-1],
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories
+    }
+    return obj
 
 
 # Requisito 3
