824ab9f23eae7838a094e786d8d10c35156832ce
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-05-14 16:03:43 -0300

    req 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -21,7 +21,44 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    """Preenche um dicinário a partir das informações
+    de uma notícia extraídas a partir do seu HTML"""
+    from parsel import Selector
+
+    selector = Selector(html_content)
+    return {
+        "url": selector.css("head link[rel=canonical]::attr(href)").get(),
+        "title": selector.css(".tec--article__header__title::text").get(),
+        "timestamp": selector.css(
+            ".tec--timestamp__item time::attr(datetime)"
+        ).get(),
+        "writer": selector.css(".tec--author__info__link::text").get().strip(),
+        "shares_count": int(
+            selector.css(".tec--toolbar__item *::text")[0]
+            .get()
+            .strip()
+            .split()[0]
+        ),
+        "comments_count": int(
+            selector.css(".tec--toolbar__item *::text")[2]
+            .get()
+            .strip()
+            .split()[0]
+        ),
+        "summary": "".join(
+            selector.css(".tec--article__body *::text").getall()
+        ),
+        "sources": [
+            source.strip()
+            for source in selector.css(".z--mb-16 .tec--badge::text").getall()
+        ],
+        "categories": [
+            category.strip()
+            for category in selector.css(
+                ".tec--badge--primary *::text"
+            ).getall()
+        ],
+    }
 
 
 # Requisito 3
