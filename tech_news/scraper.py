36ae7b38f63055c58ba046bda539ad24a847ddf6
Rangel Paolo <paolorangel1996@outlook.com>
2021-05-13 22:49:00 -0300

    Fetch function implemented

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests
+from time import sleep
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        if (response.status_code == 200):
+            return response.text
+        else:
+            return None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
