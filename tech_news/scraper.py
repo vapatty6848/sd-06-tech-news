1b0076be8dde654b766a5856efb4e7f69be85add
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-05-14 17:43:39 -0300

    Feat: Second requirement working

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import requests
 import time
 from parsel import Selector
@@ -19,12 +20,49 @@ def fetch(url):
 # Requisito 2
 def scrape_noticia(html_content):
     selector = Selector(html_content)
-    # cardTitleSelector = "h3.tec--card__title"
-    timeStampSelector = "div.tec--timestamp__item::text"
-    # url = selector.css(f"{cardTitleSelector} a::attr(href)").getall()
-    # title = selector.css(f"{cardTitleSelector} a::text").getall()
-    timestamp = selector.css(f"{timeStampSelector}").getall()
-    print(timestamp)
+
+    url = selector.css('[rel="canonical"]::attr(href)').get()
+    title = selector.css('.tec--article__header__title::text').get()
+    timestamp = selector.css('#js-article-date::attr(datetime)').get()
+    shares_count = int(
+        selector.css('.tec--toolbar__item::text')
+        .get()
+        .split()[0]
+    )
+    comments_count = int(
+        selector.css('.tec--toolbar__item *::text')
+        .getall()[2].split()[0]
+    )
+    summary = selector.css('.tec--article__body z--px-16 p402_premium')
+    writer = selector.css('.tec--author__info__link::text').get().strip()
+    url = selector.css('[rel="canonical"]::attr(href)').get()
+    summary = ''.join(
+        selector.css('.tec--article__body p:first-child *::text')
+        .getall()
+    )
+    sources = [
+        i.strip() for i in selector.css('[rel="noopener nofollow"]::text')
+        .getall()
+    ]
+
+    categories = [
+        i.strip() for i in selector.css('#js-categories a::text')
+        .getall()
+    ]
+
+    result = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "writer": writer,
+        "sources": sources,
+        "categories": categories,
+    }
+
+    return result
 
 
 # Requisito 3
@@ -43,4 +81,4 @@ def get_tech_news(amount):
 
 
 if __name__ == "__main__":
-    scrape_noticia(fetch("https://www.tecmundo.com.br/novidades"))
+    scrape_noticia(fetch("https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/155000-musk-tesla-carros-totalmente-autonomos.htm"))
