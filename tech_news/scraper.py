63ee62cf5290589bab7b871166f81dec32631bd5
luizmacmillan <luizmacmillan@gmail.com>
2021-05-14 02:21:32 -0300

    Requisito 1.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,23 @@
+import requests
+import time
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    """Pega as notícias através da URL informada"""
+    try:
+        time.sleep(1)
+        response = requests.get(url, timeout=3)
+        response.raise_for_status()
+        return response.text
+    except requests.exceptions.ConnectionError as error:
+        print(f"Erro na requisição: {error}.")
+        return None
+    except requests.exceptions.ReadTimeout as error:
+        print(f"Sem resposta do servidor: {error}.")
+        return None
+    except Exception:
+        print("Erro interno.")
+        return None
 
 
 # Requisito 2
