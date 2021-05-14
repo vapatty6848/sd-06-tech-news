47363331b79c4f5424c965f96c50601e3c2c5bb1
Tiago Berwanger <berwangertiago@gmail.com>
2021-05-13 23:37:09 -0300

    requirement 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -73,7 +73,14 @@ def scrape_novidades(html_content):
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    url_next_page = selector.xpath(
+        "//*[@id='js-main']/div/div/div[1]/div[2]/a/@href"
+    ).get()
+    if (url_next_page == ""):
+        return None
+    else: 
+        return url_next_page
 
 
 # Requisito 5
