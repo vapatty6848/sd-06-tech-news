60a8dc8e3244767302e8e185f264786a9d8ece06
Leonardo Cavachini <cavachini240@gmail.com>
2021-05-14 09:38:16 -0300

    req1-done // req-2-ongoing

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,49 @@
 # Requisito 1
+import requests
+import time
+from parsel import Selector
+
+
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=2)
+        time.sleep(1)
+        response.raise_for_status()
+    except Exception:
+        return None
+    else:
+        return response.text
 
 
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    Selector(html_content)
+    title = Selector.css(".tec--article__header__title::text").get().strip()
+    timestamp = (
+        Selector.css(".tec--timestamp tec--timestamp--lg::text").get().strip()
+    )
+    writer = Selector.css(".tec--author__info__link::text").get().strip()
+    shares_count = Selector.css(".tec-toolbar__item::text").get()
+    comments_count = Selector.css(".comment-count::text").get()
+    summary = (
+        Selector.css(".tec--article__body z--px-16 p402_premium::text")
+        .get()
+        .split()[0]
+    )
+    sources = Selector.css("tec-badge::text").get()
+    categories = Selector.css("#js-categories *::text").get()
+    return {
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
 
 
 # Requisito 3
