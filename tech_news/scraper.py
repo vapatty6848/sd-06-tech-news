d7e26f07e9f69b4ba997c243a9f5d144b5086958
Havyner Caetano <hrcgeo@gmail.com>
2021-05-14 17:56:24 -0300

    feat: requirement 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,22 @@
+import requests
+import time
+
+url = "https://www.betrybe.com/"
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(url, timeout=1)
 
+        if (response.status_code != 200):
+            return None
+
+        return response.text
+    except requests.ReadTimeout:
+        requests.ReadTimeout
 
 # Requisito 2
 def scrape_noticia(html_content):
