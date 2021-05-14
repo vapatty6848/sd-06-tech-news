b53541fedce1748d071d53f8bdaaf1211e872204
Cyrano webdev <cyrano@cyranowebdev.com>
2021-05-14 18:23:09 -0300

    feat: req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,21 @@
+from tech_news.database import search_news
+import re
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    """Faz buscas nas notícias por título no DB"""
+    try:
+        search = title.lower()
+    except AttributeError:
+        print('Please use a valid string.')
+        return None
+
+    regex = re.compile(rf'{search}', re.IGNORECASE)
+    query = {'title': {'$regex': regex}}
+    results_raw = search_news(query)
+    results = [(result['title'], result['url']) for result in results_raw]
+    return results
 
 
 # Requisito 7
