49fbc246c9b0ed727077c53761bdef416965d505
luizmacmillan <luizmacmillan@gmail.com>
2021-05-14 19:21:46 -0300

    Requisito 2 corrigido.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -19,7 +19,6 @@ def fetch(url):
         print(f"Sem resposta do servidor: {error}.")
         return None
     except Exception:
-        print("Erro interno.")
         return None
 
 
@@ -29,29 +28,33 @@ def scrape_noticia(html_content):
     Recebe o conteúdo html/txt de uma página e
     busca notícias para preencher um dicionário
     """
-    selector = Selector(text=html_content.text)
-    url = html_content.url
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
     title = selector.css(".tec--article__header__title::text").get()
-    timestamp = selector.css("""
-    .tec--timestamp__item time::attr(datetime)
-    """).get()
+    timestamp = selector.css(
+        ".tec--timestamp__item time::attr(datetime)"
+    ).get()
     writer = selector.css(".tec--author__info__link::text").get()
     shares_count_string = selector.css(".tec--toolbar__item::text").get()
-    shares_count = int(re.sub('[^0-9]', '', shares_count_string))
-    comments_count = int(selector.css("""
-    #js-comments-btn::attr(data-count)
-    """).get())
-    summary = "".join(selector.css("""
-    div.tec--article__body > p:nth-child(1) *::text
-    """).getall())
+    shares_count = int(re.sub("[^0-9]", "", shares_count_string))
+    comments_count = int(
+        selector.css("#js-comments-btn::attr(data-count)").get()
+    )
+    summary = "".join(
+        selector.css(
+            "div.tec--article__body > p:nth-child(1) *::text"
+        ).getall()
+    )
     sources = selector.css("div.z--mb-16.z--px-16 a.tec--badge::text").getall()
+    sources = [source.strip() for source in sources]
     categories = selector.css("div#js-categories a::text").getall()
+    categories = [categorie.strip() for categorie in categories]
     return {
         "url": url,
         "title": title,
         "timestamp": timestamp,
-        "writer": None if writer == '' else writer,
-        "shares_count": 0 if shares_count == '' else shares_count,
+        "writer": None if writer == "" else writer.strip(),
+        "shares_count": 0 if shares_count == "" else shares_count,
         "comments_count": comments_count,
         "summary": summary,
         "sources": sources,
