5adf5fe19ae010d8584cfa503927a84d2fd50ac9
luizmacmillan <luizmacmillan@gmail.com>
2021-05-15 00:36:42 -0300

    Requisito 10.

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,18 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    """Lista as cinco notícias mais populares."""
+    news_list = find_news()
+    popularity_list = [[
+        int(news["shares_count"]) + int(news["comments_count"]),
+        news["title"],
+        news["url"],
+        ] for news in news_list]
+    ordered_list = sorted(popularity_list, reverse=True)
+    result = [(ordere_news[1], ordere_news[2]) for ordere_news in ordered_list[:5]]
+    return result
 
 
 # Requisito 11
