f335fc1844c74b988ba108751040ea3cb6dd1156
Cyrano webdev <cyrano@cyranowebdev.com>
2021-05-15 12:17:19 -0300

    feat: req11 em mongoDB

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,4 @@
-# from tech_news.database import get_collection
+from tech_news.database import get_collection
 from tech_news.analyzer.search_engine import format_results, search_news
 from operator import itemgetter
 
@@ -47,18 +47,38 @@ def top_5_news():
     return top_5_news
 
 
-# Requisito 11
+# Requisito 11 usando Python
+# def top_5_categories():
+#     """Lista as cinco categorias mais populares;
+#         o critério é a soma dos compartilhamentos e comentários"""
+#     news_raw = search_news({})
+
+#     categories_count = {}
+#     for news in news_raw:
+#         for category in news['categories']:
+#             categories_count.setdefault(category, 0)
+#             categories_count[category] += 1
+
+#     categories_sort = sorted(categories_count)
+#     top_5_categories = categories_sort[:5]
+#     return top_5_categories
+
+
+# Requisito 11 usando MongoDB
 def top_5_categories():
     """Lista as cinco categorias mais populares;
         o critério é a soma dos compartilhamentos e comentários"""
-    news_raw = search_news({})
-
-    categories_count = {}
-    for news in news_raw:
-        for category in news['categories']:
-            categories_count.setdefault(category, 0)
-            categories_count[category] += 1
-
-    categories_sort = sorted(categories_count)
-    top_5_categories = categories_sort[:5]
+    news_collection = get_collection()
+    db_result = list(news_collection.aggregate([
+        {'$unwind': '$categories'},
+        {
+            '$group': {
+                '_id': '$categories',
+                'count': {'$sum': 1}
+            }
+        },
+        {'$sort': {'count': -1, '_id': 1}},
+        {'$limit': 5}
+    ]))
+    top_5_categories = [category['_id'] for category in db_result]
     return top_5_categories
