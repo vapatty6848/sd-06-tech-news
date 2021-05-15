35159a2c57eee9533cc5171368843c47a74e9f82
rafMewtwo <raf.ponciano@gmail.com>
2021-05-15 18:44:21 -0300

    req 5 problem

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -3,6 +3,7 @@ import requests
 import time
 from parsel import Selector
 from requests.exceptions import ReadTimeout
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -23,19 +24,40 @@ def fetch(url):
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
     seletor = Selector(html_content)
+
     writer_by = seletor.css('.tec--author__info__link::text').get()
+    if writer_by is None:
+        writer_by = seletor.css(
+            '.tec--timestamp__item.z--font-bold>a::text'
+        ).get()
+        if writer_by is not None:
+            writer_by = writer_by.strip()
+    else:
+        writer_by = writer_by.strip()
+
+    shares_count_info = seletor.css(
+            '.tec--toolbar__item::text'
+        ).get()
+    if shares_count_info is not None:
+        shares_count_info = int(shares_count_info.split()[0])
+    else:
+        shares_count_info = 0
+
+    comments_count_info = seletor.css(
+            '.tec--toolbar__item *::text'
+        ).getall()
+    if len(comments_count_info) == 3:
+        comments_count_info = int(comments_count_info[2].split()[0])
+    else:
+        comments_count_info = int(comments_count_info[1].split()[0])
 
     return {
         "url": seletor.css('[rel="canonical"]::attr(href)').get(),
         "title": seletor.css('.tec--article__header__title::text').get(),
         "timestamp": seletor.css('#js-article-date::attr(datetime)').get(),
-        "writer": writer_by.strip(),
-        "shares_count": int(seletor.css(
-            '.tec--toolbar__item::text'
-        ).get().split()[0]),
-        "comments_count": int(seletor.css(
-            '.tec--toolbar__item *::text'
-        ).getall()[2].split()[0]),
+        "writer": writer_by,
+        "shares_count": shares_count_info,
+        "comments_count": comments_count_info,
         "summary": ''.join(seletor.css(
             '.tec--article__body p:first-child *::text'
         ).getall()),
@@ -71,15 +93,29 @@ def scrape_next_page_link(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
-    # url = 'https://www.tecmundo.com.br/novidades'
-    # html_news = fetch(url)
-    # lista_news = scrape_novidades(html_news)
-    # news = []
-    # while (len(news) < amount):
-    #     html_att = fetch(lista_news[len(news)])
-    #     obj_data = scrape_noticia(html_att)
-    #     news.append(obj_data)
-    # print(news)
+    news_to_save = []
+    url = "https://www.tecmundo.com.br/novidades"
+    news_page = fetch(url)
+    News_list = scrape_novidades(news_page)
+    print(news_page)
+
+    while len(news_to_save) < amount:
+        for news in News_list:
+            news_html = fetch(news)
+            news_to_save.append(scrape_noticia(news_html))
+            if len(news_to_save) == amount:
+                break
+        next_url = scrape_next_page_link(news_page)
+        news_page = fetch(next_url)
+        News_list = scrape_novidades(news_page)
+
+    create_news(news_to_save)
+
+    return news_to_save
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    print(get_tech_news(3))
+    # htmlgg = fetch('https://www.tecmundo.com.br/redes-sociais/217386-encontrar-grupos-telegram.htm')
+    # print(htmlgg)
+    # print(scrape_noticia(htmlgg))
