029e2e4d5eaf4cfad8291ee8c977ee2e51efe2fa
Ana Flávia <anaflvia@gmail.com>
2021-05-15 14:09:36 -0300

    requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,19 @@
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    import requests
+    import time
 
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+    except requests.ReadTimeout:
+        return None
+
+    if (response.status_code == 200):
+        return response.text
+    else:
+        return None
 
 # Requisito 2
 def scrape_noticia(html_content):
