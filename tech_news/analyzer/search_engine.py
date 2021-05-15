f323019291e4cf381cf45c09c9389fc21ab55198
danieleperse <danieleperse3@gmail.com>
2021-05-15 16:22:56 -0300

    req. 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -35,7 +35,13 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    find_source = search_news({"sources": {"$regex": source, "$options": "i"}})
+
+    if find_source:
+        for new in find_source:
+            return [(new["title"], new["url"])]
+    else:
+        return []
 
 
 # Requisito 9
