011f8cb2f45f8cc4d8f4a920deacac750725aace
Dilenio Enderle <dilenio@gmail.com>
2021-05-16 12:07:19 -0300

    feat: request 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    """Method to search news by title"""
+
+    title_news = search_news({"title": {"$regex": title, "$options": "i"}})
+    if title_news:
+        for news in title_news:
+            return [(news["title"], news["url"])]
+    else:
+        return []
 
 
 # Requisito 7
