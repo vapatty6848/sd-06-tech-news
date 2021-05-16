ee21e6362dfaf896c848883da0e3025fdd62f6f3
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-05-15 23:24:38 -0300

    req 6-9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,49 @@
-# Requisito 6
+from functools import reduce
+from datetime import datetime
+from tech_news.database import find_news
+
+
+def search_news_param(to_search, obj_key):
+    def handle_search(param):
+        return (
+            [n.casefold() for n in param]
+            if type(param) == list
+            else param.casefold()
+        )
+
+    news = find_news()
+    return [
+        reduce(
+            lambda a, b: a + (b["title"], b["url"]),
+            filter(
+                lambda x: to_search.casefold() in handle_search(x[obj_key]),
+                news,
+            ),
+            (),
+        )
+    ]
+
+
 def search_by_title(title):
-    
+    result = search_news_param(title, "title")
+    return [] if len(result[0]) == 0 else result
 
 
-# Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        bool(datetime.strptime(date, "%Y-%m-%d"))
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    result = search_news_param(date, "timestamp")
+    return [] if len(result[0]) == 0 else result
 
 
-# Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    result = search_news_param(source, "sources")
+    return [] if len(result[0]) == 0 else result
 
 
-# Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    result = search_news_param(category, "categories")
+    return [] if len(result[0]) == 0 else result
