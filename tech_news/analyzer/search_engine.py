b5f55d3cc5a544beb21909701d7c4319aaf1670e
Dilenio Enderle <dilenio@gmail.com>
2021-05-16 12:16:36 -0300

    feat: request 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,13 @@
 from tech_news.database import search_news
+import datetime
 
 
 # Requisito 6
 def search_by_title(title):
     """Method to search news by title"""
 
-    title_news = search_news({"title": {"$regex": title, "$options": "i"}})
+    query = {"title": {"$regex": title, "$options": "i"}}
+    title_news = search_news(query)
     if title_news:
         for news in title_news:
             return [(news["title"], news["url"])]
@@ -15,7 +17,17 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    """Method to search news by date"""
+
+    try:
+        datetime.datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    query = {"timestamp": {"$regex": date}}
+    query_result = search_news(query)
+    result = [(news["title"], news["url"]) for news in query_result]
+    return result
 
 
 # Requisito 8
