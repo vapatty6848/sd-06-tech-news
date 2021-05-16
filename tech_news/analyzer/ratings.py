c294c953470b8c139c3c60a82b416e8992ebf060
rafMewtwo <raf.ponciano@gmail.com>
2021-05-16 18:30:04 -0300

    req ate o 11 pela metade

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,29 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    # montando a estrutura para fazer uma lista com todas as news
+    all_news = find_news()
+    news_top_popularity = []
+    for news in all_news:
+        popularity = news['comments_count'] + news['shares_count']
+        new = {
+            'title': news['title'],
+            'url': news['url'],
+            'popularity': popularity
+        }
+        news_top_popularity.append(new)
+    # montar uma estrutura para selecionar as 5 com maior pop
 
 
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+    all_news = find_news()
+    all_categories = []
+    for news in all_news:
+        for category in news["categories"]:
+            all_categories.append(category)
+    # preciso agora selecionar as catego mais repetidas da list all_categories
--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -4,20 +4,27 @@ from tech_news.database import search_news
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
-    post_by_title = search_news({"title": title})
+    post_by_title = search_news({"title": {"$regex": title}})
     return post_by_title
 
 
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    # falta arranjar uma forma de converter o formato recebido p/ timestamp
+    post_by_date = search_news({"source": {"$regex": date}})
+    return post_by_date
 
 
 # Requisito 8
 def search_by_source(source):
     """Seu código deve vir aqui"""
+    post_by_source = search_news({"source": {"$regex": source}})
+    return post_by_source
 
 
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    post_by_cat = search_news({"categories": {"$regex": category}})
+    return post_by_cat
