c5199f785d576637ee67638a93da4f7f634166db
Daniel Moura <darthur.moura@gmail.com>
2021-05-15 23:13:47 -0300

    Req 2 passing local tests, fixed flake issues

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -10,7 +10,7 @@ def fetch(url):
     time.sleep(1)
     try:
         r = requests.get(url, timeout=3)
-        r.raise_for_status() # Raises exception if status != 200
+        r.raise_for_status()  # Raises exception if status != 200
         page = r.text
     except (requests.HTTPError, requests.ReadTimeout) as e:
         # As seen on Stackoverflow: https://tinyurl.com/yb4pnwa6
@@ -22,7 +22,41 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    title = selector.css('h1::text').get()
+    url = selector.xpath('//link[contains(@rel, "canonical")]/@href').get()
+    writer_css = selector.css('p > a::text').get()
+    writer = writer_css.strip() if not None else None
+    timestamp = selector.css('#js-article-date::attr(datetime)').get()
+    shares_count = int(selector.xpath(
+        '//nav/div[contains(., "Compartilharam")]/text()'
+        ).get().strip().split()[0])
+    comments_count = int(selector.css(
+        '#js-comments-btn::text'
+        ).getall()[1].strip().split()[0])
+    dirty_summary = selector.css('.tec--article__body p').get()
+    cleaner = re.compile('<.*?>')
+    summary = re.sub(cleaner, '', dirty_summary)
+    dirty_sources = selector.css('.z--mb-16 .tec--badge::text').getall()
+    sources = []
+    for s in dirty_sources:
+        sources.append(s.strip())
+    dirty_categories = selector.css('#js-categories a::text').getall()
+    categories = []
+    for a in dirty_categories:
+        categories.append(a.strip())
+    news = {
+        'url': url,
+        'title': title,
+        'writer': writer,
+        'summary': summary,
+        'timestamp': timestamp,
+        'shares_count': shares_count,
+        'comments_count': comments_count,
+        'sources': sources,
+        'categories': categories,
+    }
+    return news
 
 
 # Requisito 3
