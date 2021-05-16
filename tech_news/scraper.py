499af3c2c6f32620a16177e498a42a7802680ee0
Daniel Moura <darthur.moura@gmail.com>
2021-05-16 18:10:16 -0300

    Req 5 tests are crazy, lost precious time here

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,4 +1,5 @@
 from parsel import Selector
+from tech_news.database import create_news
 import requests
 import re
 import time
@@ -25,18 +26,21 @@ def scrape_noticia(html_content):
     selector = Selector(text=html_content)
     title = selector.css('h1::text').get()
     url = selector.xpath('//link[contains(@rel, "canonical")]/@href').get()
-    writer_css = selector.css('p > a::text').get()
-    writer = writer_css.strip() if not None else None
+    writer_css = selector.css('.tec--author__info > p > a::text').get()
+    writer = writer_css.strip() if writer_css else None
     timestamp = selector.css('#js-article-date::attr(datetime)').get()
-    shares_count = int(selector.xpath(
+    shares_count = selector.xpath(
         '//nav/div[contains(., "Compartilharam")]/text()'
-        ).get().strip().split()[0])
+        ).get()
+    shares_count = int(re.findall(
+        r'\d+', shares_count)[0]
+        ) if shares_count is not None else 0
     comments_count = int(selector.css(
         '#js-comments-btn::text'
         ).getall()[1].strip().split()[0])
     dirty_summary = selector.css('.tec--article__body p').get()
     cleaner = re.compile('<.*?>')
-    summary = re.sub(cleaner, '', dirty_summary)
+    summary = re.sub(cleaner, '', dirty_summary).replace('&amp;', '&')
     dirty_sources = selector.css('.z--mb-16 .tec--badge::text').getall()
     sources = []
     for s in dirty_sources:
@@ -75,4 +79,44 @@ def scrape_next_page_link(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    base_url = 'https://www.tecmundo.com.br/novidades'
+    news_qty = int(amount)
+
+    links = []
+    while len(links) < news_qty:
+        news_list_html = fetch(base_url)
+        links.extend(scrape_novidades(news_list_html))
+        base_url = scrape_next_page_link(news_list_html)
+
+    news_list = []
+    for i in range(len(links) - 1):
+        while len(news_list) < news_qty:
+            news_html = fetch(links[i])
+            scraped = scrape_noticia(news_html)
+            news_list.append(scraped)
+            i += 1
+
+    create_news(news_list)
+
+    return news_list
+
+
+# %%
+# from tech_news.scraper import fetch, scrape_novidades, scrape_next_page_link, scrape_noticia
+# base_url = fetch('https://www.tecmundo.com.br/novidades?page=')
+# news_qty = 30
+
+# links = scrape_novidades(page)
+# news = []
+# for news_article in links:
+#     news_html = fetch(news_article)
+#     scraped = scrape_noticia(news_html)
+#     print(scraped['url'])
+#     news.append(scraped)
+# create_news(news)
+
+# %%
+# from tech_news.scraper import fetch, scrape_novidades, scrape_next_page_link, scrape_noticia, get_tech_news
+
+# get_tech_news(30)
+# %%
