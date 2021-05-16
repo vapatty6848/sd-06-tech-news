e0ac84768bb998321a377fb4739d49400377c694
Havyner Caetano <hrcgeo@gmail.com>
2021-05-16 15:24:32 -0300

    feat: requirement 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -9,7 +10,7 @@ def fetch(url):
         time.sleep(1)
         response = requests.get(url, timeout=1)
 
-        if (response.status_code != 200):
+        if response.status_code != 200:
             return None
 
         return response.text
@@ -20,6 +21,45 @@ def fetch(url):
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    s = Selector(text=html_content)
+    url = s.xpath('//link[@rel="amphtml"]').get().split('href="')[1]
+    url = url.split('">')[0]
+    url = url.replace("amp/", "")
+    title = s.css(".tec--article__header__title::text").get()
+    timestamp = s.css("time").xpath("@datetime").get()
+    writer = s.css(".tec--author__info__link::text").get().strip()
+    shares_element = s.css(".tec--toolbar__item::text").getall()[0]
+    shares_count = shares_element.split(" ")[1].split(" ")[0]
+    shares_count = int(shares_count)
+    comments_element = s.css("#js-comments-btn::text").getall()[1]
+    comments_count = comments_element.split(" ")[1].split(" ")[0]
+    comments_count = int(comments_count)
+    summary_selector = "div.tec--article__body > p:nth-child(1) *::text"
+    summary_content = s.css(summary_selector).getall()
+    summary = "".join(summary_content)
+    sources = []
+    sources_list = s.css(
+        ".tec--article__body-grid .z--mb-16 div a::text"
+    ).getall()
+    for source in sources_list:
+        sources.append(source.strip())
+    categories = []
+    categories_list = s.css("#js-categories a::text").getall()
+    for category in categories_list:
+        categories.append(category.strip())
+    scraped_news = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
+
+    return scraped_news
 
 
 # Requisito 3
