6fe1febfc146f2614f7be27cedfbd3a80bc1142f
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-05-17 18:38:45 -0300

    Requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,21 @@
+from tech_news.database import search_news
+import re
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    """A função deve buscar as notícias do banco de dados por título"""
+
+    #  para atender o case insensitive
+    #  re.IGNORECASE permite a correspondência sem distinção entre
+    #  maiúsculas e minúsculas da Expressão Regular com a string fornecida
+    #  https://www.geeksforgeeks.org/name-validation-using-ignorecase-in-python-regex/
+    #  https://www.tutorialspoint.com/How-to-write-a-case-insensitive-Python-regular-expression-without-re-compile
+    #  avaliar outra alternativa era transformar .lower()
+    query = {"title": {"$regex": re.compile(title, re.IGNORECASE)}}
+    search_in_db = search_news(query)
+    news_founded = [(news["title"], news["url"]) for news in search_in_db]
+    return news_founded
 
 
 # Requisito 7
