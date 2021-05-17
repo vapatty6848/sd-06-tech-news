0f76b0e58efe6d092071826849cc10c56be5fe33
DehSartoreto <fixsartoreto@gmail.com>
2021-05-17 13:01:13 -0300

    req3 e 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -21,6 +21,7 @@ def fetch(url):
 # Requisito 2
 def scrape_noticia(html_content):
     selector = Selector(text=html_content)
+
     url = selector.css("head link[rel=canonical]::attr(href)").get()
     title = selector.css("#js-article-title::text").get()
     timestamp = selector.css("#js-article-date::attr(datetime)").get()
@@ -37,11 +38,13 @@ def scrape_noticia(html_content):
     summary = selector.css(
         ".tec--article__body > p:nth-child(1) *::text"
     ).getall()
+
     summary_new = "".join(summary)
     sources = selector.css(".z--mb-16 .tec--badge::text").getall()
     sources_new = [source.strip() for source in sources]
     categories = selector.css("#js-categories > a *::text").getall()
     categories_new = [category.strip() for category in categories]
+
     allNews = {
         "url": url,
         "title": title,
@@ -53,17 +56,26 @@ def scrape_noticia(html_content):
         "sources": sources_new,
         "categories": categories_new,
     }
+
     return allNews
 
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    newsSection = selector.css(
+        '.tec--list a.tec--card__title__link::attr(href)'
+    ).getall()
+
+    return newsSection
 
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    nextPage = selector.css('.tec--list > a::attr(href)').get() or None
+
+    return nextPage
 
 
 # Requisito 5
