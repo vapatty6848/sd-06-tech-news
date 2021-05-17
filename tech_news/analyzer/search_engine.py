4a13cbb2a87bec58bd174fde321f907f2b5d379e
LeandroParisi <lepcbelisario@gmail.com>
2021-05-17 16:30:45 -0300

    feat: requirement 7 implemented

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,34 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+
+def validate_date(date_text):
+    try:
+        datetime.strptime(date_text, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
+def format_news(news):
+    return [(new["title"], new["url"]) for new in news]
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    search_query = {"title": {"$regex": title, "$options": "i"}}
+    found_news = search_news(search_query)
+    formatted_news = format_news(found_news)
+    return formatted_news
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    validate_date(date)
+    search_query = {"timestamp": {"$regex": f"^{date}", "$options": "m"}}
+    found_news = search_news(search_query)
+    formatted_news = format_news(found_news)
+    print(formatted_news)
+    return formatted_news
 
 
 # Requisito 8
