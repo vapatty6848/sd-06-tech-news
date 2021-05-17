97bab45e8ebf577e0763a1bd6ce0fcb994c6f2ff
alcantara2509 <alcantara2509@gmail.com>
2021-05-16 22:55:48 -0300

    req 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,17 @@
-# Requisito 1
+import requests
+import time
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+    except requests.ReadTimeout:
+        return None
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
 
 
 # Requisito 2
