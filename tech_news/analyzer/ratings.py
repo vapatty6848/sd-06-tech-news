1520f86bde5d0a8cacd1341f4d26d7f74b559ec9
thalesgr <thalesgr@gmail.com>
2021-05-17 16:59:19 -0300

    req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,22 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    popularity_list = [
+        [
+            int(news["shares_count"]) + int(news["comments_count"]),
+            news["title"],
+            news["url"],
+        ]
+        for news in news_list
+    ]
+    ordered_list = sorted(popularity_list, reverse=True)
+    result = [
+        (ordered_news[1], ordered_news[2]) for ordered_news in ordered_list[:5]
+    ]
+    return result
 
 
 # Requisito 11
