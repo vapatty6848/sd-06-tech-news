f5eb9af6deb72e4abe84c58ae44d794caf4cf9b6
Marcelo Pessini <marceloppessini@gmail.com>
2021-05-17 17:33:23 -0300

    flakes power

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,7 @@
 import requests
 import time
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
@@ -11,6 +13,8 @@ def fetch(url):
     if response.status_code != 200:
         return None
     return response.text
+
+
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
