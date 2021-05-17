6c0248b7b46df2edc3b6f2c1159eac48bd5a1f27
rafMewtwo <raf.ponciano@gmail.com>
2021-05-16 21:49:33 -0300

    req 6 regex

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -5,7 +5,9 @@ from tech_news.database import search_news
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
-    post_by_title = search_news({"title": {"$regex": title}})
+    regex_title = f"/^{title}$/i"
+    print(regex_title)
+    post_by_title = search_news({"title": regex_title})
     # map para tirar e formatar o titulo e url do array
     post_result = [(post["title"], post["url"]) for post in post_by_title]
     return post_result
