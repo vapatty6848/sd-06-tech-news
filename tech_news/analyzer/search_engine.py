55be32473ae823ddf76903c56fff23945c40ebce
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-05-17 19:21:18 -0300

    Requisitos 7 ao 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 from tech_news.database import search_news
 import re
+import datetime
 
 
 # Requisito 6
@@ -20,14 +21,35 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    """Esta função irá buscar as notícias do banco de dados por data"""
+    # https://stackoverflow.com/questions/55961092/how-to-use-striptime-y-m-d-hms-minus-1-minute
+    # https://python.hotexamples.com/pt/examples/mx/DateTime/strptime/python-datetime-strptime-method-examples.html
+    try:
+        datetime.datetime.strptime(date, "%Y-%m-%d")
+
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    query = {"timestamp": {"$regex": date}}
+    search_in_db = search_news(query)
+    news_founded = [(news["title"], news["url"]) for news in search_in_db]
+    return news_founded
 
 
+# Os 2 próximos requisitos são aprecidos com o req 6
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    """função irá buscar as notícias por fonte"""
+    query = {"sources": {"$regex": re.compile(source, re.IGNORECASE)}}
+    search_in_db = search_news(query)
+    news_founded = [(news["title"], news["url"]) for news in search_in_db]
+    return news_founded
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    """função irá buscar as notícias por categoria"""
+    query = {"categories": {"$regex": re.compile(category, re.IGNORECASE)}}
+    search_in_db = search_news(query)
+    news_founded = [(news["title"], news["url"]) for news in search_in_db]
+    return news_founded
