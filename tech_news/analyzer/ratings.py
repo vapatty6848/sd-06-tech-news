4298e64c939bb020443e36a2ee5a3c9868a5cb91
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-05-18 00:10:55 -0300

    feat: add list of the top 5 most poupular news feature

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,37 @@
+from pymongo import MongoClient
+from decouple import config
+
+
+DB_HOST = config("DB_HOST", default="localhost")
+DB_PORT = config("DB_PORT", default="27017")
+
+client = MongoClient(host=DB_HOST, port=int(DB_PORT))
+db = client.tech_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    top_5_news = []
+    query_result = db.news.aggregate(
+        [
+            {
+                "$project": {
+                    "_id": False,
+                    "title": True,
+                    "url": True,
+                    "popularity": {
+                        "$add": ["$shares_count", "$comments_count"]
+                    },
+                }
+            },
+            {"$sort": {"popularity": -1, "title": 1}},
+            {"$limit": 5},
+        ]
+    )
+    for result in query_result:
+        news_info = (result["title"], result["url"])
+        top_5_news.append(news_info)
+    return top_5_news
 
 
 # Requisito 11
