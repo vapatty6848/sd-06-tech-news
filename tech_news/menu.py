40ce95ec992b0f26f9bf637c62e446435b83b1a4
Cyrano webdev <cyrano@cyranowebdev.com>
2021-05-18 12:12:22 -0300

    tentando req12

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -9,70 +9,87 @@ from tech_news.analyzer.search_engine import (
 from tech_news.analyzer.ratings import top_5_news, top_5_categories
 
 
-def validate_option(input):
-    try:
-        option = int(input)
-    except Exception:
-        return print("Opção inválida", file=sys.stderr)
-
-    if 0 < option > 7:
-        print("Opção inválida", file=sys.stderr)
-
-    return option
+def end_script():
+    print('Encerrando script')
 
 
-# Requisito 12
+MENU = {
+    0: {
+        'menu': '0 - Popular o banco com notícias;\n',
+        'input': 'Digite quantas notícias serão buscadas: ',
+        'action': get_tech_news,
+        },
+    1: {
+        'menu': '1 - Buscar notícias por título;\n',
+        'input': 'Digite o título: ',
+        'action': search_by_title,
+        },
+    2: {
+        'menu': '2 - Buscar noticias por data;\n',
+        'input': 'Digite a data no formato aaaa-mm-dd: ',
+        'action': search_by_date,
+        },
+    3: {
+        'menu': '3 - Buscar notícias por fonte;\n',
+        'input': 'Digite a fonte: ',
+        'action': search_by_source,
+        },
+    4: {
+        'menu': '4 - Buscar notícias por categoria;\n',
+        'input': 'Digite a categoria: ',
+        'action': search_by_category,
+        },
+    5: {
+        'menu': '5 - Listar top 5 notícias;\n',
+        'action': top_5_news,
+        },
+    6: {
+        'menu': '6 - Listar top 5 categorias;\n',
+        'action': top_5_categories,
+        },
+    7: {
+        'menu': '7 - Sair.',
+        'action': end_script,
+        },
+}
+
+
+def initiate_menu():
+    user_menu = ''.join([f' {MENU[key]["menu"]}' for key in MENU])
+    user_input = input(
+        f'Selecione uma das opções a seguir:\n{user_menu}\n')
+    return int(user_input)
+
+
+def handle_parameter(input, choice):
+    return int(input) if choice == 0 else input
+
+
+def validate_option(choice):
+    if choice not in range(8):
+        raise ValueError("Opção inválida")
+
+
 def analyzer_menu():
-    """Menu do programa"""
+    try:
+        user_choice = initiate_menu()
+        validate_option(user_choice)
+    except ValueError:
+        return print('Opção inválida', file=sys.stderr)
+    except KeyboardInterrupt:
+        return end_script()
 
-    # menu_options = """
-    #     Selecione uma das opções a seguir:
-    #      0 - Popular banco;
-    #      1 - Buscar notícias por título;
-    #      2 - Buscar notícias por data;
-    #      3 - Buscar notícias por fonte;
-    #      4 - Buscar notícias por categoria;
-    #      5 - Listar top 5 notícias;
-    #      6 - Listar top 5 categorias;
-    #      7 - Sair.
-    #     """
-
-    menu_options = "Selecione uma das opções a seguir:\n"\
-        " 0 - Popular banco;\n"\
-        " 1 - Buscar notícias por título;\n"\
-        " 2 - Buscar notícias por data;\n"\
-        " 3 - Buscar notícias por fonte;\n"\
-        " 4 - Buscar notícias por categoria;\n"\
-        " 5 - Listar top 5 notícias;\n"\
-        " 6 - Listar top 5 categorias;\n"\
-        " 7 - Sair."\
-
-    menu_input = input(menu_options)
-    option = validate_option(menu_input)
-    run_menu_option(option)
+    if user_choice in (0, 1, 2, 3, 4):
+        user_action = MENU[user_choice]['input']
+        parameter = handle_parameter(input(user_action), user_choice)
+        output = MENU[user_choice]["action"](parameter)
+        return print(output)
+    elif user_choice != 7:
+        output = MENU[user_choice]["action"]()
+        print(output)
+    else:
+        end_script()
 
 
-def run_menu_option(option):
-    if option == 0:
-        news_quantity = input('Digite quantas notícias serão buscadas:')
-        return get_tech_news(news_quantity)
-    elif option == 1:
-        title = input('Digite o título:')
-        query = search_by_title(title)
-        print(query)
-        return query
-    elif option == 2:
-        date = input('Digite a data no formato aaaa-mm-dd:')
-        return search_by_date(date)
-    elif option == 3:
-        source = input('Digite a fonte:')
-        return search_by_source(source)
-    elif option == 4:
-        category = input('Digite a categoria:')
-        return search_by_category(category)
-    elif option == 5:
-        return top_5_news()
-    elif option == 6:
-        return top_5_categories()
-    elif option == 7:
-        return print('Encerrando script')
+if __name__ == "__main__":
+    analyzer_menu()
