b81fdfac9219ac7aafe0dc8fa7409ab82ee0bfaf
Leandro Feitoza <leandro.feitoza8018@gmail.com>
2021-05-18 20:15:42 -0300

    atualização na master

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,8 @@
 # Requisito 1
 import requests
 import time
+import re
+from parsel import Selector
 
 
 def fetch(url):
@@ -18,7 +20,55 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    url = selector.css("#js-main h3 a::attr(href)").get()
+    title = selector.css(".tec--article__header__title::text").get().strip()
+    timestamp = selector.css("#js-article-date ::attr(datetime)").get().strip()
+    check_writer = selector.css(".tec--author p a::text").get().strip()
+    if (check_writer) is not None:
+        writer = check_writer
+    else:
+        writer = None
+    check_shares_count = int(
+        (selector.css(".tec--toolbar__item::text").get().split()[0])
+    )
+    if (check_shares_count) is not None:
+        shares_count = int(check_shares_count)
+    else:
+        shares_count = None
+    comments_count = int(
+        selector.css("#js-comments-btn ::attr(data-count)").get().strip()
+    )
+    get_summary = " ".join(
+        selector.css(".tec--article__body p:nth-child(1) ::text").getall()
+    )
+    no_comma = re.sub(r'\s+([?,!"])', r"\1", get_summary)
+    summary = no_comma.replace("  ", " ")
+
+    list_sources = selector.css(
+        "#js-main div.z--mb-16.z--px-16 a::text"
+    ).getall()
+    sources = []
+    for source in list_sources:
+        sources.append(source.strip())
+
+    list_categories = selector.css("#js-categories a::text").getall()
+    categories = []
+    for categorie in list_categories:
+        categories.append(categorie.strip())
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
 
 
 # Requisito 3
