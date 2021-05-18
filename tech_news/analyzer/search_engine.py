7e7a57e49fd6aff00007d34e23d0f1af98550d7b
Rangel Paolo <paolorangel1996@outlook.com>
2021-05-17 21:26:46 -0300

    Requirement 7 done

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 from tech_news.database import search_news
 import re
+import datetime
 
 
 # Requisito 6
@@ -15,7 +16,17 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    query = {
+        "timestamp": {
+            "$regex": date
+        }
+    }
+    response = search_news(query)
+    return [(item["title"], item["url"]) for item in response]
 
 
 # Requisito 8
@@ -28,4 +39,5 @@ def search_by_category(category):
     """Seu código deve vir aqui"""
 
 
-# print(search_by_title("que"))
+# print(search_by_date("2020-11-23"))
+# print(search_by_date("21-12-1980"))
