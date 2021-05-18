347ebf5527b402bf35a4ffe35ac3cfb38a3873c7
ricardorosa-dev <ricardorosa@gmail.com>
2021-05-17 22:44:11 -0300

    Ainda estou preso no requisito 2. Nao consigo entender por que o teste nao passa

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,7 @@ import time
 import requests
 from requests.exceptions import HTTPError, ReadTimeout
 from parsel import Selector
+import pprint
 
 
 # Requisito 1
@@ -24,46 +25,66 @@ def fetch(url):
     return response.text
 
 
-response = fetch("https://www.tecmundo.com.br/novidades")
+# response = fetch("https://www.tecmundo.com.br/dispositivos-moveis/215327-pixel-5a-tera-lancamento-limitado-devido-escassez-chips.htm")
 # print(response)
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
-    selector = Selector(html_content)
-    # Isso é pra uma noticia. Faz um for em todas
-    element = {
-        "url": selector.css(".tec--card__title__link::attr(href)")[0].get(),
-        "title": selector.css(".tec--card__title__link::text")[0]
-        .get().strip(),
-    }
+    new_selector = Selector(html_content)
 
-    # faz uma busca pelo html de dentro da noticia
-    news_details = fetch(element['url'])
-    new_selector = Selector(news_details)
+    element = {
+        # "url": selector.css(".tec--card__title__link::attr(href)")[0].get(),
+        "url": new_selector.css("link::attr(href)")[18].getall(),
+    }
+    # new_selector.css("link::attr(href)")
+
+    # news_details = fetch(element['url'])
+    # new_selector = Selector(news_details)
+    
+    element['title'] = new_selector.css(
+      ".tec--article__header__title::text").get()
     element['timestamp'] = new_selector.css(
         ".tec--timestamp__item time::attr(datetime)").get()
-    element['writer'] = new_selector.css(
-        ".tec--author__info__link::text").get() or None
-    element['shares_count'] = int(new_selector.css(
-        ".tec--toolbar__item::text").get().strip().split()[0]) or 0
-    element['comments_count'] = int(new_selector.css(
-        "#js-comments-btn::text").getall()[1].strip().split()[0])
-    time.sleep(1)
-    element['summary'] = new_selector.css(
-        ".tec--article__body.z--px-16.p402_premium p::text").getall()[1]
-    element['sources'] = new_selector.css(
+
+    writer = new_selector.css(
+        ".tec--author__info__link::text").get()
+    element['writer'] = writer.strip() if writer is not None else None
+
+    shares_count = new_selector.css(
+        ".tec--toolbar__item::text").get()
+    shares_count = shares_count.strip().split()[0] if shares_count is not None else 0
+    element['shares_count'] = int(shares_count)
+
+    comments_count = new_selector.css(
+        "#js-comments-btn *::text").getall()
+    comments_count = comments_count[1].strip().split(' ')[0] if comments_count is not None else 0
+    element['comments_count'] = int(comments_count)
+
+    summary = new_selector.css(
+        ".tec--article__body.z--px-16.p402_premium > p:first-child::text").getall()
+    separator = ""
+    summary = separator.join(summary)
+    element['summary'] = summary
+
+    sources = new_selector.css(
         ".z--mb-16.z--px-16 a::text").getall()
-    for item in element['sources']:
-        item = item.strip()
-    element['categories'] = new_selector.css(
+    for i in range(len(sources)):
+        sources[i] = sources[i].strip()
+    element['sources'] = sources
+    
+    categories = new_selector.css(
         "#js-categories a::text").getall()
-    print(element)
+    for i in range(len(categories)):
+        categories[i] = categories[i].strip()
+    element['categories'] = categories
+
+    return str(element)
 
 
+pp = pprint.PrettyPrinter(indent=4)
 file = scrape_noticia(response)
-# print(file)
+pp.pprint(file)
 
 
 # Requisito 3
