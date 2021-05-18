38c4e922f5f004a74a426eab52fccca7e0763797
labelle <beelsdelonge@gmail.com>
2021-05-17 21:21:37 -0300

    Nao sei mais o que fazer do requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -8,40 +8,35 @@ def fetch(url):
     try:
         time.sleep(1)
         response = requests.get(url, timeout=3)
-        return response.text
+        return response.text if response.status_code == 200 else None
     except requests.ReadTimeout:
-        response = requests.get(url, timeout=3)
         return None
-    return response.text if response.status_code == 200 else None
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    response = requests.get(html_content)
-    selector = Selector(text=response.text)
-    all_urls = selector.css("link::attr(href)").getall()
-    url = all_urls[-2]
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
     title = selector.css(".tec--article__header__title::text").get()
     timestamp = selector.css(".tec--timestamp__item time ::attr(datetime)").get()
     writer = selector.css(".tec--author__info__link::text").get()
-    shares = selector.css(".tec--toolbar__item::text").get()
-    shares_count = shares[0:3]
+    shares = selector.css(".tec--toolbar__item::text").re_first(r"\d+")
+    shares_count = int(shares) if shares else 0
     comments = selector.css(".tec--btn::attr(data-count)").get()
-    not_clean_summary = selector.css("div.tec--article__body > p:nth-child(1) *::text").getall()
-    summary = "".join(not_clean_summary)
-    all_sources = selector.css(".tec--badge::text").getall()
-    sources = all_sources[0:1]
-    categories = all_sources[1:5]
+    uncl_summary = selector.css("div.tec--article__body > p:nth-child(1) *::text").getall()
+    summary = "".join(uncl_summary)
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    categories = selector.css("#js-categories > a *::text").getall()
     return {
         "url": url,
         "title": title,
         "timestamp": timestamp,
         "writer": writer,
-        "shares_count": int(shares_count),
-        "comments_count": int(comments),
+        "shares_count": shares_count,
+        "comments_count": comments,
         "summary": summary,
-        "sources": list(sources),
-        "categories": list(categories),
+        "sources": sources,
+        "categories": categories,
     }
 
 
