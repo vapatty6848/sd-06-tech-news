8938aa1df029fe4cf7d3578df80b67331eadf103
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-05-18 16:30:39 -0300

    :sparkles: Requirement 02

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,10 @@
 import time
 import requests
 from requests.exceptions import ReadTimeout, HTTPError
+from parsel import Selector
+
+with open('/home/alexander/Downloads/teste.html', 'r') as file:
+    texto = file.read()
 
 
 # Requisito 1
@@ -22,7 +26,43 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    url = selector.css('[rel=canonical]::attr(href)').get()
+
+    title = selector.css('h1::text').get()
+
+    writer = selector.css('.tec--author__info__link::text').get().strip()
+
+    timestamp = selector.css('time::attr(datetime)').get()
+
+    shares = int(selector.css(
+        '.tec--toolbar__item::text')[0].getall()[0].split(' ')[1])
+
+    comments = int(selector.css(
+            '.tec--btn::text').getall()[9].split(' ')[1])
+
+    summary_fetch = selector.css(
+        '.tec--article__body p:first-child *::text').getall()
+    summary = "".join(summary_fetch)
+
+    sourcesList = selector.css('a[class=tec--badge]::text').getall()
+    sources = [source.strip() for source in sourcesList]
+
+    categoriesList = selector.css('.tec--badge--primary::text').getall()
+    categories = [categorie.strip() for categorie in categoriesList]
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares,
+        "comments_count": comments,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories
+    }
 
 
 # Requisito 3
@@ -38,3 +78,6 @@ def scrape_next_page_link(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+print(scrape_noticia(texto))
