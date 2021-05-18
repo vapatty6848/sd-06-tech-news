bb9b61d10482452b414bf8d882f5cf1d4a1724bd
Alvaro Neto <alvaroshn@gmail.com>
2021-05-18 10:37:24 -0300

    req 1,2,3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,16 +1,71 @@
 # Requisito 1
+import time
+import requests
+from parsel import Selector
+
+
+# Remover espaços
+def remove_spaces(list):
+    return [string.strip() for string in list if len(string) > 1]
+
+
+# Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        response.raise_for_status()
+    except Exception:
+        return None
+    else:
+        return response.text
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("head > link[rel=canonical]::attr(href)").get()
+    title = selector.css("h1.tec--article__header__title ::text").get()
+    timestamp = selector.css(
+        "div.tec--timestamp__item time::attr(datetime)"
+    ).get()
+    writer = selector.css("div.tec--author__info a::text").get()
+    shares_count = int(
+        selector.css("div.tec--toolbar__item ::text").get()[1:3]
+    )
+    comments_count = int(
+        selector.css("#js-comments-btn ::attr(data-count)").get()
+    )
+    summary = selector.css(
+        ".tec--article__body p:first-child *::text"
+    ).getall()
+    fixed_summary = "".join(summary)
+    sources = remove_spaces(selector.css("div.z--mb-16 div *::text").getall())
+    categories = remove_spaces(
+        selector.css("#js-categories a.tec--badge *::text").getall()
+    )
+
+    scraped_infos = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": fixed_summary,
+        "sources": sources,
+        "categories": categories,
+    }
+    return scraped_infos
 
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    novidades = selector.css(
+        ".tec--list a.tec--card__title__link::attr(href)"
+    ).getall()
+    return novidades
 
 
 # Requisito 4
---
+++ tech_news/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+scrape_noticia(
+    fetch(
+        "https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/155000-musk-tesla-carros-totalmente-autonomos.htm"
+    )
+)
