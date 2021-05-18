82d7f6b82c0265f0d545b4ffd317c713e652c535
Alvaro Neto <alvaroshn@gmail.com>
2021-05-18 10:41:52 -0300

    error on tempCodeRunnerFile

--- tech_news/tempCodeRunnerFile.py
+++ tech_news/tempCodeRunnerFile.py
@@ -1,5 +1,9 @@
+from tech_news import scrape_noticia
+from tech_news import fetch
+
 scrape_noticia(
     fetch(
-        "https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/155000-musk-tesla-carros-totalmente-autonomos.htm"
+        "https://www.tecmundo.com.br/mobilidade-urbana-smart-cities"
+        + "/155000-musk-tesla-carros-totalmente-autonomos.htm"
     )
 )
