3a934434e882cae7a8e4c69ffcefe58e360a512a
Cyrano webdev <cyrano@cyranowebdev.com>
2021-05-18 14:10:05 -0300

    refat: menu

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -10,7 +10,7 @@ from tech_news.analyzer.ratings import top_5_news, top_5_categories
 
 
 def end_script():
-    print('Encerrando script')
+    return 'Encerrando script'
 
 
 MENU = {
@@ -18,38 +18,46 @@ MENU = {
         'menu': '0 - Popular o banco com notícias;\n',
         'input': 'Digite quantas notícias serão buscadas: ',
         'action': get_tech_news,
+        'has_params': True,
         },
     1: {
         'menu': '1 - Buscar notícias por título;\n',
         'input': 'Digite o título: ',
         'action': search_by_title,
+        'has_params': True,
         },
     2: {
         'menu': '2 - Buscar noticias por data;\n',
         'input': 'Digite a data no formato aaaa-mm-dd: ',
         'action': search_by_date,
+        'has_params': True,
         },
     3: {
         'menu': '3 - Buscar notícias por fonte;\n',
         'input': 'Digite a fonte: ',
         'action': search_by_source,
+        'has_params': True,
         },
     4: {
         'menu': '4 - Buscar notícias por categoria;\n',
         'input': 'Digite a categoria: ',
         'action': search_by_category,
+        'has_params': True,
         },
     5: {
         'menu': '5 - Listar top 5 notícias;\n',
         'action': top_5_news,
+        'has_params': False,
         },
     6: {
         'menu': '6 - Listar top 5 categorias;\n',
         'action': top_5_categories,
+        'has_params': False,
         },
     7: {
         'menu': '7 - Sair.',
         'action': end_script,
+        'has_params': False,
         },
 }
 
@@ -58,7 +66,8 @@ def initiate_menu():
     user_menu = ''.join([f' {MENU[key]["menu"]}' for key in MENU])
     user_input = input(
         f'Selecione uma das opções a seguir:\n{user_menu}\n')
-    return int(user_input)
+    get_number_input = ''.join(char for char in user_input if char.isdigit())
+    return int(get_number_input)
 
 
 def handle_parameter(input, choice):
@@ -68,27 +77,27 @@ def handle_parameter(input, choice):
 def validate_option(choice):
     if choice not in range(8):
         raise ValueError("Opção inválida")
-    if choice == 7:
-        raise KeyboardInterrupt()
 
 
 def analyzer_menu():
     try:
         user_choice = initiate_menu()
         validate_option(user_choice)
+
+        if MENU[user_choice]['has_params']:
+            user_action = MENU[user_choice]['input']
+            parameter = handle_parameter(input(user_action), user_choice)
+            output = MENU[user_choice]["action"](parameter)
+            return print(output)
+        else:
+            output = MENU[user_choice]["action"]()
+            print(output)
+
     except ValueError:
         return print('Opção inválida', file=sys.stderr)
     except KeyboardInterrupt:
+        print()
         return end_script()
-
-    if user_choice in (0, 1, 2, 3, 4):
-        user_action = MENU[user_choice]['input']
-        parameter = handle_parameter(input(user_action), user_choice)
-        output = MENU[user_choice]["action"](parameter)
-        return print(output)
-    else:
-        output = MENU[user_choice]["action"]()
-        print(output)
 
 
 if __name__ == "__main__":
