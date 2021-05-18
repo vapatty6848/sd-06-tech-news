917b7e1280c165cc819908f640b0f07ac72f53e6
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-05-18 16:40:07 -0300

    :bug: Requirement 02 Fix 01

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -3,9 +3,6 @@ import requests
 from requests.exceptions import ReadTimeout, HTTPError
 from parsel import Selector
 
-with open('/home/alexander/Downloads/teste.html', 'r') as file:
-    texto = file.read()
-
 
 # Requisito 1
 def fetch(url):
@@ -78,6 +75,3 @@ def scrape_next_page_link(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
-
-
-print(scrape_noticia(texto))
