351640a98aa95115d4d83500d282b722f2f293bd
Rangel Paolo <paolorangel1996@outlook.com>
2021-05-17 22:12:11 -0300

    Requirement 10 with effort

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,29 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    top_five_most_popular = sorted([
+        [
+            int(
+                act_news["shares_count"]
+            )
+            +
+            int(
+                act_news["comments_count"]
+            ),
+            act_news["title"],
+            act_news["url"],
+        ]
+        for act_news in news
+    ], reverse=True)
+    return [(news[1], news[2]) for news in top_five_most_popular[:5]]
 
 
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+
+
+# print(top_5_news())
