87f9872dda5a43d8f93d55f551f828ef34645d2f
Cyrano webdev <cyrano@cyranowebdev.com>
2021-05-19 15:21:57 -0300

    fix(menu): menu usando dict e loop - req 12 ok

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -15,48 +15,48 @@ def end_script():
 
 MENU = {
     0: {
-        'menu': '0 - Popular o banco com notícias;',
+        'menu': 'Popular o banco com notícias;',
         'input': 'Digite quantas notícias serão buscadas: ',
         'action': get_tech_news,
         'has_params': True,
         'params_to_int': True,
         },
     1: {
-        'menu': '1 - Buscar notícias por título;',
+        'menu': 'Buscar notícias por título;',
         'input': 'Digite o título: ',
         'action': search_by_title,
         'has_params': True,
         },
     2: {
-        'menu': '2 - Buscar noticias por data;',
+        'menu': 'Buscar notícias por data;',
         'input': 'Digite a data no formato aaaa-mm-dd: ',
         'action': search_by_date,
         'has_params': True,
         },
     3: {
-        'menu': '3 - Buscar notícias por fonte;',
+        'menu': 'Buscar notícias por fonte;',
         'input': 'Digite a fonte: ',
         'action': search_by_source,
         'has_params': True,
         },
     4: {
-        'menu': '4 - Buscar notícias por categoria;',
+        'menu': 'Buscar notícias por categoria;',
         'input': 'Digite a categoria: ',
         'action': search_by_category,
         'has_params': True,
         },
     5: {
-        'menu': '5 - Listar top 5 notícias;',
+        'menu': 'Listar top 5 notícias;',
         'action': top_5_news,
         'has_params': False,
         },
     6: {
-        'menu': '6 - Listar top 5 categorias;',
+        'menu': 'Listar top 5 categorias;',
         'action': top_5_categories,
         'has_params': False,
         },
     7: {
-        'menu': '7 - Sair.',
+        'menu': 'Sair.',
         'action': end_script,
         'has_params': False,
         },
@@ -64,17 +64,8 @@ MENU = {
 
 
 def initiate_menu():
-    # user_menu = ''.join([f' {MENU[key]["menu"]}\n' for key in MENU])
-    print('Selecione uma das opções a seguir:')
-    print(' 0 - Popular o banco com notícias;')
-    print(' 1 - Buscar notícias por título;')
-    print(' 2 - Buscar notícias por data;')
-    print(' 3 - Buscar notícias por fonte;')
-    print(' 4 - Buscar notícias por categoria;')
-    print(' 5 - Listar top 5 notícias;')
-    print(' 6 - Listar top 5 categorias;')
-    print(' 7 - Sair.')
-    user_input = input()
+    user_menu = '\n'.join([f' {key} - {MENU[key]["menu"]}' for key in MENU])
+    user_input = input(f'Selecione uma das opções a seguir:\n{user_menu}')
     try:
         get_number_input = ''.join(
             char for char in user_input if char.isdigit())
