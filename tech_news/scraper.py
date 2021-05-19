049e488e31061c0bcc9f27c4182f4f50c6f5b23e
Usuario <mguimaraes.carneiro@gmail.com>
2021-05-19 16:07:12 -0300

    Tech News | Requirement 01 | FIX 02

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -3,30 +3,31 @@ import requests
 from requests.exceptions import ReadTimeout
 
 # Requisito 1 start
+# Esta função será responsável por fazer a requisição HTTP ao site Tecmundo e obter o conteúdo HTML
 def fetch(url):
-    time.sleep(1)
-    try:
-        response = requests.get(url, timeout=3)
-    except ReadTimeout:
-        return None
+  time.sleep(1)
+  try:
+    response = requests.get(url, timeout=3)
+  except ReadTimeout:
+    return None
 
-    return response.text if response.status_code == 200 else None
+  return response.text if response.status_code == 200 else None
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+  """Seu código deve vir aqui"""
 
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+  """Seu código deve vir aqui"""
 
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+  """Seu código deve vir aqui"""
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+  """Seu código deve vir aqui"""
