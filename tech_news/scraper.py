08aaa6aeff32aa0b40d14c9e88e9b211ecf2b452
labelle <beelsdelonge@gmail.com>
2021-05-19 15:04:45 -0300

    Requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -57,7 +57,11 @@ def scrape_novidades(html_content):
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page = selector.css(
+        "#js-main > div > div > .z--w-2-3 > div.tec--list--lg > a::attr(href)"
+    ).get()
+    return next_page
 
 
 # Requisito 5
