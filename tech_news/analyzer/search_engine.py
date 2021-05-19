c25906a0e487d42c6e7d1c287759a5b12242198e
Fernando Luckesi <fernando.luckesi@94gmail.com>
2021-05-19 16:29:32 -0300

    Requisito 7 finalizado

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,21 +1,36 @@
 from tech_news.database import db
+from datetime import datetime
 
-# from pymongo import MongoClient
+from pymongo import MongoClient
 
 
 # Requisito 6
 def search_by_title(title):
-    # client = MongoClient("mongodb://localhost:27017/")
-    # db = client.library
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client.tech_news
     list_notices = []
-    for new in db.books.find({"title": {"$regex": title}}):
-        list_notices.append([(new["title"], new["_id"])])
+    for new in db.news.find({"title": {"$regex": title}}):
+        list_notices.append((new["title"], new["url"]))
+    print(list_notices)
     return list_notices
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client.tech_news
+    try:
+        new_date = datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        return "Data inválida"
+    else:
+        list_notices = []
+        print(new_date)
+        for new in db.news.find({"timestamp": {"$regex": date}}):
+            list_notices.append((new["title"], new["_id"]))
+        print(list_notices)
+
+search_by_date("201-05-19")
 
 
 # Requisito 8
