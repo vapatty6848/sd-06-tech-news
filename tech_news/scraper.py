a681b455e910a891d17473fb46c1643b5b29a26f
CristianoNunes <cristianonunes99@gmail.com>
2021-05-19 16:50:46 -0300

    Requisito 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,7 @@ import time
 import requests
 from requests.exceptions import Timeout
 import parsel
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -73,4 +74,16 @@ def scrape_next_page_link(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    url = "https://www.tecmundo.com.br/novidades"
+    data = []
+    while url:
+        response = fetch(url)
+        list = scrape_novidades(response)
+        for notice in list:
+            response_news = fetch(notice)
+            item = scrape_noticia(response_news)
+            data.append(item)
+            if amount == len(data):
+                create_news(data)
+                return data
+        url = scrape_next_page_link(response)
