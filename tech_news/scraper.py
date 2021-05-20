5a1cd7d5319646f7e29985b46dd56dee1f2cf863
RenatoCostaDev <renatotrybe@gmail.com>
2021-05-20 09:21:53 -0300

    feat:Requisito-2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -19,6 +20,51 @@ def fetch(url):
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    url_selector = selector.css("head > link:nth-child(26)::attr(href)").get()
+    title_selector = selector.css("#js-article-title::text").get()
+    time_stamp_selector = selector.css(
+        "#js-article-date::attr(datetime)"
+    ).get()
+    writer_selector = selector.css(
+        "#js-author-bar > div > p.z--m-none.z--truncate.z--font-bold > a::text"
+    ).get()
+    writer_name = writer_selector.strip() if writer_selector else None
+    count_selector = selector.css(
+        "#js-author-bar > nav > div:nth-child(1)::text"
+    ).get()
+    shares_count = int(count_selector.split()[0]) if count_selector else 0
+    comments_count_selector = selector.css(
+        "#js-comments-btn::attr(data-count)"
+    ).get()
+    comments = (
+        int(comments_count_selector) if comments_count_selector else None
+    )
+    summary_selector = selector.css(
+        "#js-main div.tec--article__body p:nth-child(1) *::text"
+    ).getall()
+    summary_all = "".join(summary_selector)
+    sources_selector = selector.css(
+        "#js-main div.tec--article__body-grid > div.z--mb-16 > div > a::text"
+    ).getall()
+    sources_list = [source.strip() for source in sources_selector]
+    categories_selector = selector.css("#js-categories > a::text").getall()
+    categories_list = [category.strip() for category in categories_selector]
+
+    new_data = {
+        "url": url_selector,
+        "title": title_selector,
+        "timestamp": time_stamp_selector,
+        "writer": writer_name,
+        "shares_count": shares_count,
+        "comments_count": comments,
+        "summary": summary_all,
+        "sources": sources_list,
+        "categories": categories_list,
+    }
+
+    return new_data
 
 
 # Requisito 3
