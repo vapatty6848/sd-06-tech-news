2b8747cee0c1126f9653cfb15c10164e73170b5d
procopio420 <lucas-procopio@outlook.com>
2021-05-20 19:59:27 -0300

    lint estranho da porra

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -3,14 +3,13 @@ import requests
 from parsel import Selector
 from tech_news.database import create_news
 
+
 def fetch(url):
     time.sleep(1)
-    try:
-        response = requests.get(url, timeout=3)
-    except ReadTimeout:
-        return None
+    response = requests.get(url, timeout=3)
 
     return response.text if response.status_code == 200 else None
+
 
 def scrape_noticia(html_content):
     selector = Selector(text=html_content)
@@ -32,7 +31,7 @@ def scrape_noticia(html_content):
     get_sources = selector.css(".z--mb-16 .tec--badge::text").getall()
     get_categories = selector.css("#js-categories > a *::text").getall()
 
-    writer = writer.strip() if writer else None
+    writer = get_writer.strip() if get_writer else None
     shares = int(get_shares_count) if get_shares_count else 0
     comments = int(get_comments) if get_comments else None
     summary = "".join(get_summary)
@@ -53,6 +52,7 @@ def scrape_noticia(html_content):
 
     return dic_news
 
+
 def scrape_novidades(html_content):
     if html_content == "":
         return []
@@ -61,12 +61,14 @@ def scrape_novidades(html_content):
         get_news = selector.css(".tec--card__info h3 a::attr(href)").getall()
         return get_news
 
+
 def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
     get_next_url = selector.css(
         "#js-main > div > div > .z--w-2-3 > div.tec--list--lg > a::attr(href)"
     ).get()
     return get_next_url
+
 
 def get_tech_news(amount):
     url = "https://www.tecmundo.com.br/novidades"
@@ -82,6 +84,7 @@ def get_tech_news(amount):
                 create_news(news)
                 return news
         url = scrape_next_page_link(response)
+
 
 if __name__ == "__main__":
     get_tech_news(15)
