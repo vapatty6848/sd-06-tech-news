05bfac36b05fd1fcc410f46d23d4bb36a20bb15f
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-05-21 00:23:11 -0300

    feat: Requirements 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,10 @@
+from tech_news.database import create_news
 import requests
 from requests.exceptions import Timeout
 import time
 from parsel import Selector
 import re
 
-
-url = "https://www.tecmundo.com.br/novidades"
 
 # Requisito 1
 
@@ -22,24 +21,6 @@ def fetch(url):
         return None
 
 
-# {
-#   "url": "https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/155000-musk-tesla-carros-totalmente-autonomos.htm",
-#   "title": "Musk: Tesla está muito perto de carros totalmente autônomos",
-#   "timestamp": "2020-07-09T11:00:00",
-#   "writer": "Nilton Kleina",
-#   "shares_count": 61,
-#   "comments_count": 26,
-#   "summary": "O CEO da Tesla, Elon Musk, garantiu que a montadora está muito perto de atingir o chamado nível 5 de autonomia de sistemas de piloto automático de carros. A informação foi confirmada em uma mensagem enviada pelo executivo aos participantes da Conferência Anual de Inteligência Artificial (WAIC, na sigla em inglês). O evento aconteceu em Xangai, na China, onde a montadora comemora resultados positivos de mercado.",
-#   "sources": ["Venture Beat"],
-#   "categories": [
-#     "Mobilidade Urbana/Smart Cities",
-#     "Veículos autônomos",
-#     "Tesla",
-#     "Elon Musk"
-#   ]
-# }
-
-
 # Requisito 2
 
 
@@ -48,27 +29,25 @@ def scrape_noticia(html_content):
     url = selector.css("head link[rel=canonical]::attr(href)").get()
     title = selector.css(".tec--article__header__title::text").get()
     timestamp = selector.css("#js-article-date::attr(datetime)").get()
-    writer = (
-        selector.css(
-            "#js-author-bar > div > p.z--m-none.z--truncate.z--font-bold > a::text"
-        )
-        .get()
-        .strip()
-    )
+    writer = selector.css(
+        "#js-author-bar > div > p.z--m-none.z--truncate.z--font-bold > a::text"
+    ).get()
+    writer = selector.css(
+        "#js-author-bar > div > p.z--m-none.z--truncate.z--font-bold > a::text"
+    ).get()
+    writer = writer.strip() if writer else None
     shares_count = selector.css(
         "#js-author-bar > nav > div:nth-child(1)::text"
     ).get()
-    shares_count = re.sub("[^0-9]", "", shares_count)
+    shares_count = shares_count.split(" ")[1] if shares_count else 0
     comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
     summary = selector.css(
         ".tec--article__body p:nth-child(1) *::text"
     ).getall()
     summary = "".join(summary)
-    sources = selector.css(
-        "#js-main > div.z--container > article > div.tec--article__body-grid > div.z--mb-16.z--px-16 > div > a.tec--badge::text"
-    ).getall()
-    categories = selector.css("#js-categories > a.tec--badge::text").getall()
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
     sources = [source.strip() for source in sources]
+    categories = selector.css("#js-categories > a.tec--badge::text").getall()
     categories = [category.strip() for category in categories]
 
     news = {
@@ -84,12 +63,6 @@ def scrape_noticia(html_content):
     }
 
     return news
-
-
-html = fetch(
-    "https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/155000-musk-tesla-carros-totalmente-autonomos.htm"
-)
-print(scrape_noticia(html))
 
 
 # Requisito 3
@@ -112,4 +85,23 @@ def scrape_next_page_link(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    url = "https://www.tecmundo.com.br/novidades"
+    urls = []
+
+    while len(urls) < amount:
+        request = fetch(url)
+        urls.extend(scrape_novidades(request))
+        url = scrape_next_page_link(request)
+
+    news_group = urls[:amount]
+    news_list = []
+    for news in news_group:
+        news_list.append(scrape_noticia(fetch(news)))
+
+    try:
+        create_news(news_list)
+
+    except Exception as error:
+        print(error)
+
+    return news_list
