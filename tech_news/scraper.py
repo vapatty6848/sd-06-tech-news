1a6519062782bf9517f4cd80952e68974bbeb0b6
Renan Cunha <renan2610@gmail.com>
2021-05-20 21:41:31 -0300

    feat: adding requirement 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 # "https://www.tecmundo.com.br/novidades"
 # Requisito 1
@@ -20,7 +21,38 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("head link[rel=canonical]::attr(href)").get()
+    title = selector.css(".tec--article__header__title::text").get()
+    timestamp = selector.css(
+        ".tec--timestamp__item time::attr(datetime)"
+    ).get()
+    writer = selector.css(".tec--author__info__link::text").get()
+    writer = writer.strip()
+    shares_count = selector.css(".tec--toolbar__item::text").get()
+    shares_count = int(shares_count.split(" ")[1]) if shares_count else 0
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    comments_count = int(comments_count) if comments_count else None
+    summary = selector.css(
+        ".tec--article__body p:nth-child(1) *::text"
+    ).getall()
+    summary = "".join(summary)
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    sources = [source.strip() for source in sources]
+    categories = selector.css("#js-categories a::text").getall()
+    categories = [category.strip() for category in categories]
+    dic_data = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories
+    }
+    return dic_data
 
 
 # Requisito 3
